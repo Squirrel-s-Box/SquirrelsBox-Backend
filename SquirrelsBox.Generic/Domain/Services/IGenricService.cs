3d6438a334bd593b667ec22a98fcfe3a7c14578d
DiegoHZ5 <49615431+Diego5HZ@users.noreply.github.com>
2023-12-10 18:39:24 -0500

    Add project files.

---
+++ SquirrelsBox.Generic/Domain/Services/IGenricService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquirrelsBox.Generic.Domain.Services
+{
+    public interface IGenricService<T, R>
+    {
+        Task<R> SaveAsync(T model);
+        Task<R> FindByIdAsync(int id);
+        Task<R> FindByCodeAsync(string value);
+        Task<R> UpdateAsync(int id, T model);
+        Task<R> DeleteAsync(int id);
+    }
+}
---
+++ SquirrelsBox.Generic/Extensions/ErrorMessagesExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquirrelsBox.Generic.Extensions
+{
+    public static class ErrorMessagesExtensions
+    {
+        public static List<string> GetErrorMessages(List<KeyValuePair<string, object>> errorState)
+        {
+            var errors = new List<string>();
+            foreach (var item in errorState)
+            {   
+                if (item.Value is List<string> errorList)
+                {
+                    errors.AddRange(errorList);
+                }
+            }
+            return errors;
+        }
+    }
+}
